9e8c1c5c3e0a40b93ddc4ccbd9b9b77c14741e96
Semyon <125258352+SSUHOY@users.noreply.github.com>
2024-01-29 18:05:25 +0700

    creates gameboard layout and styles

---
+++ src/components/shared/Logo.tsx
@@ -0,0 +1,10 @@
+import { SnakeLogo } from "../../styles/MainPage/Main.styles";
+
+export const Logo = () => {
+  return (
+    <SnakeLogo
+      src={require("./../../assets/images/snake-game-ai-gen.png")}
+      alt="Snake-logo"
+    />
+  );
+};
--- src/styles/MainPage/Main.styles.ts
+++ src/styles/MainPage/Main.styles.ts
@@ -1,31 +1,53 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-  padding-left: calc(50% - 600px);
-  padding-right: calc(50% - 600px);
-`;
+export const Container = styled.div``;
 
-export const Header = styled.header`
+export const Scores = styled.div`
   display: flex;
-  flex-direction: column;
-  align-items: center;
+  justify-content: space-between;
 `;
 
-export const HeaderTitle = styled.h1`
-  font-size: 32px;
-  padding-bottom: 36px;
-`;
+export const Score = styled.h1``;
+export const HighScore = styled.h1``;
 
-export const PostsBox = styled.div`
-  background-color: black;
+export const GameBorderExternal = styled.div`
+  border: #595f43 solid 10px;
+  border-radius: 30px;
+  box-shadow: inset 0 0 0 10px #595f43;
 `;
-export const PostsItems = styled.div`
-  width: 1200px;
-  height: 320px;
-  max-width: 1158px;
-  width: 100%;
+export const GameBorderInternal = styled.div`
+  border: #abb78a solid 8px;
+  border-radius: 26px;
+  box-shadow: inset 0 0 0 10px #abb78a;
+`;
+export const GameBorderInsideInternal = styled.div`
+  border: #8b966c solid 30px;
+  border-radius: 20px;
+  box-shadow: inset 0 0 0 5px #8b966c;
+`;
+export const GameBoard = styled.div`
   display: grid;
-  grid-template-columns: repeat(4, 270px);
-  grid-auto-rows: 441px;
-  grid-gap: 40px 26px;
+  grid-template-columns: repeat(20, 20px);
+  grid-template-rows: repeat(20, 20px);
+  margin: 5px;
+  background-color: #b5bf93;
+`;
+
+export const Instruction = styled.div`
+  position: relative;
+  color: #333;
+  bottom: 438px;
+  font-size: 28px;
+  width: 300px;
+  left: 74px;
+  text-align: center;
+  text-transform: capitalize;
+  padding: 30px;
+  margin: 0;
+`;
+
+export const SnakeLogo = styled.img`
+  position: relative;
+  bottom: 424px;
+  left: 127px;
 `;
